3a7fd7333174a321add005b4918509c4ccf2973b
LotorSVK <edwinkatona@gmail.com>
2025-06-26 20:14:49 +0200

    int commit

--- src/main/java/TutorialPointStudentFormTest.java
+++ src/main/java/TutorialPointStudentFormTest.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,21 +20,22 @@ public class TutorialPointStudentFormTest {
 
     @Given("Uzivatel je na stranke tutorial point student form")
     public void uzivatelJeNaStrankeTutorialPointStudentForm() {
+        ovladac.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
     }
 
     @When("Uzivatel zada sve jmeno {string}")
     public void uzivatelZadaSveJmeno(String jmeno) {
-
+        ovladac.findElement(By.id("name")).sendKeys(jmeno);
     }
 
     @And("Uzivatel zada svoj email {string}")
     public void uzivatelZadaSvojEmail(String email) {
-
+        ovladac.findElement(By.id("email")).sendKeys(email);
     }
 
     @And("Uzivatel klikne na pohlavi {string}")
     public void uzivatelKlikneNaPohlavi(String pohlavi) {
-
+        ovladac.findElement(By.id("gender")).click();
     }
 
     @Then("Radiobutton male je zaskrtnute")
