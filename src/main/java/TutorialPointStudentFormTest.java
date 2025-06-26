98c57a026ef6742fe780627e13ed4f7f9fcd2afd
LotorSVK <edwinkatona@gmail.com>
2025-06-26 19:54:14 +0200

    int commit

--- src/main/java/TutorialPointStudentFormTest.java
+++ src/main/java/TutorialPointStudentFormTest.java
@@ -1,14 +1,21 @@
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TutorialPointStudentFormTest {
 
     private WebDriver ovladac;
 
-
+    @Before
+    public void totoNastanePredKazdymTestem(){
+        WebDriverManager.chromedriver().setup();
+        ovladac = new ChromeDriver();
+    }
 
     @Given("Uzivatel je na stranke tutorial point student form")
     public void uzivatelJeNaStrankeTutorialPointStudentForm() {
